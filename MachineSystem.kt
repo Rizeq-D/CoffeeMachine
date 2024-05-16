aa7fa1a92b1591e7ccbae82093530ae724a6ae17
Abdulrazak <rizeq@Abdulrazaks-MacBook-Pro.local>
2024-05-16 17:00:38 +0200

    added a user input for names and sugar cubes

--- MachineSystem.kt
+++ MachineSystem.kt
@@ -2,25 +2,48 @@ package com.rizeq.coffeemachine
 
 fun main () {
 
-    makingCoffee(1)
-    makingCoffee(3)
-    makingCoffee(5)
-    makingCoffee(0)
-    makingCoffee(-2)
+    var customerName = ""
+    var sugarCubes = 0
 
+    while (true) {
+
+    println("Please enter your name:")
+    customerName = readLine() ?: ""
+
+        if (customerName.isBlank() || !customerName.all { it.isLetter() }) {
+            println("Invalid name. Please enter a valid name consisting of only letters.")
+            continue
+        }
+
+    println("Please enter the number of sugar cubes you would like to add:")
+    sugarCubes = readLine()?.toIntOrNull() ?: -1
+
+        if (sugarCubes < 0) {
+            println("Invalid number of sugar cubes. Please enter a non-negative number.")
+            continue
+        }
+        
+        break
+    }
+
+    makingCoffee(sugarCubes, customerName)
 
 }
 
-fun makingCoffee(sugarCubes: Int) {
+fun makingCoffee(sugarCubes: Int, customerName: String) {
 
     if (sugarCubes == 1) {
-        println("You added $sugarCubes sugar cube in your coffee:)")
+        println("You added $sugarCubes sugar cube in your coffee:) \n" +
+                "coffee for $customerName")
     }else if (sugarCubes == 0) {
-        println("You did not add any sugar cubes in your coffee:)")
+        println("You did not add any sugar cubes in your coffee:), your coffee is sugar free \n" +
+                "coffee for $customerName")
     }else if (sugarCubes < 0) {
-        println("negative numbers are considered as an valid value")
+        println("negative numbers are considered as an valid value \n" +
+                "coffee for $customerName")
     }else{
-        println("You added $sugarCubes sugar cubes in your coffee:)")
+        println("You added $sugarCubes sugar cubes in your coffee:) \n" +
+                "coffee for $customerName")
     }
 
 }
